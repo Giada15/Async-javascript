5b9d99976d90cc5f2784294ce04e4e4e0bff1ebc
Giada15 <tascheriogiada@gmail.com>
2023-10-25 12:40:04 -0400

    Handle returned values from async function
    In this commit:
    -Threw an error in the catch block of the async function to mark the promise as rejected if there is an error.
    -Implemented the IIFE pattern to handle returned values from async promises.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-// promisees
-
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
@@ -21,16 +19,19 @@ const writeFilePro = (file, data) => {
   });
 };
 
-// readFilePro(`${__dirname}/dog.txt`)
-//   .then((data) => {
-//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-//   })
-//   .then((res) => {
-//     console.log(res.body.message);
-//     return writeFilePro('dog-img.txt', res.body.message);
-//   })
-//   .then(() => console.log('Random dog image saved to file'))
-//   .catch((err) => console.log(err));
+/* promises
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => console.log('Random dog image saved to file'))
+  .catch((err) => console.log(err));
+*/
 
 // async await
 
@@ -44,24 +45,49 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+
+  return '2: READY';
 };
 
-getDogPic();
+// implement IFI pattern to handle returned value from async promise
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR');
+  }
+})();
 
-// callbacks
+/* handle returned value from async promise with .then
 
-// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-//   console.log(`Breed: ${data}`);
-//   superagent
-//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-//     .end((err, res) => {
-//       if (err) return console.log(err.message);
-//       console.log(res.body.message);
+console.log('1: Will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  })
+  .catch((err) => console.log(err));
+*/
 
-//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
-//         if (err) return console.log(err.message);
-//         console.log('Random dog image saved to file');
-//       });
-//     });
-// });
+/* callbacks
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file');
+      });
+    });
+});
+*/
